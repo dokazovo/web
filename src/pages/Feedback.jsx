b4fada0c5a700290c80731f245bb95df9eebe363
Vadym Ivanenko <vadym.ivanenko@icloud.com>
2023-07-08 21:40:41 +0300

    Feedback & About pages

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,8 +1,12 @@
 function Feedback() {
   return (
-      <>
-        Feedback
-      </>
+      <div className="container">
+        <h1>Зворотній звʼязок</h1>
+        <div className="text-center">
+          <p><a href="mailto: dokazovo@gmail.com" style={{ textDecoration: 'none' }}>dokazovo@gmail.com</a></p>
+          <p><a href="https://t.me/get_random" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>@get_random</a></p>
+        </div>
+      </div>
   );
 }
 
